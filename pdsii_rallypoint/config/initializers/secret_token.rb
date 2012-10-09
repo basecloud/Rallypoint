e378363085f4dbd701261beacfdf66646d762e4d
basecloud <Basecloud.git@six3systems.com>
2012-10-09 08:35:33 -0700

    Git setup

---
+++ pdsii_rallypoint/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PdsiiRallypoint::Application.config.secret_token = 'cfc1a9f6b6a4eac971056c51a8a040372b4915835cfbaf354a49b0c9d4448bcb883f648f6bfc7600bf891b9e92ac7ac4cc4cb2c32fe40464b4da87d752efe05e'
